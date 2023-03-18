c359d14297b24e27d6321e702c4342ccc91636d7
fsimic346 <fsimic346@gmail.com>
2023-03-18 15:36:57 +0100

    akcije

---
+++ BagerMC/DTO/Action/FeedBeeWithNectar.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagerMC.DTO.Action
+{
+    public class FeedBeeWithNectar
+    {
+        [JsonProperty("playerId")]
+        public int PlayerId { get; set; }
+        [JsonProperty("gameId")]
+        public int GameId { get; set; }
+        [JsonProperty("amountOfNectarToFeedWith")]
+        public int AmountOfNectarToFeedWith { get; set; }
+    }
+}
